b6436a9e686f85a4fb394f52c6c30f643f96fbed
carlospuigserver <cpuiggut@myuax.com>
2023-11-07 13:38:13 +0100

    comit

--- EJERCICIO 1/scripts/modelado_estructuracion.py
+++ EJERCICIO 1/scripts/modelado_estructuracion.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
-# Cargar datos para modelado y estructura para análisis
+# Cargar datos desde el archivo preprocesado
 data = pd.read_csv("EJERCICIO 1/resultados/datos_preprocesados.csv", sep=';', encoding='ISO-8859-1')
 
 # Realizar modelado y estructuración para análisis
 
-# Ejemplo: Agregar una nueva columna calculada
-data['Nueva_Columna'] = data['Columna_A'] * data['Columna_B']
-
-# Ejemplo: Agrupar datos para un análisis resumido
-resumen = data.groupby('Columna_C').agg({'Columna_D': 'mean', 'Columna_E': 'sum'})
+# Ejemplo: Crear una nueva columna calculando la duración de la intervención
+data['Duracion Intervencion (min)'] = (pd.to_datetime(data['Hora Intervención']) - pd.to_datetime(data['Hora Solicitud'])).dt.total_seconds() / 60
 
 # Guardar resultados del modelado o estructuración si es necesario
-resumen.to_csv('EJERCICIO 1/resultados/resultados_analisis_resumen.csv', sep=';', encoding='ISO-8859-1')
+data.to_csv('EJERCICIO 1/resultados/datos_con_duracion.csv', sep=';', encoding='ISO-8859-1', index=False)
