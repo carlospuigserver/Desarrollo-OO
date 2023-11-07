4d8fb7655df9ffdde303e3484100e9b04d6a8daa
carlospuigserver <cpuiggut@myuax.com>
2023-11-07 14:48:23 +0100

    comit

--- EJERCICIO 1/AbstractFactoryMethod/fabrica_estadisticas.py
+++ EJERCICIO 1/AbstractFactoryMethod/fabrica_estadisticas.py
@@ -10,18 +10,49 @@ class EstadisticasFactory(ABC):
 # Producto Concreto para Resumen Estadístico
 class ResumenEstadistico(EstadisticasFactory):
     def crear_resumen_estadistico(self, datos):
-        return datos.describe()
+        # Calcula las estadísticas por separado
+        promedio = datos.mean()
+        mediana = datos.median()
+        desviacion_estandar = datos.std()
+        minimo = datos.min()
+        maximo = datos.max()
+
+        return promedio, mediana, desviacion_estandar, minimo, maximo
 
 # Función que maneja la interacción entre el patrón Abstract Factory y el conjunto de datos
 def interactuar_con_datos(producto, datos):
     resultado = producto.crear_resumen_estadistico(datos)
-    print(resultado)  # Puedes adaptar esto para guardar o mostrar los resultados de la manera que desees
+    # Presentar los resultados de la manera que desees
+    print("Resultados estadísticos de la Duracion Intervencion (min):")
+    print("Promedio:", resultado[0])
+    print("Mediana:", resultado[1])
+    print("Desviaciíon estándar:", resultado[2])
+    print("Mínimo:", resultado[3])
+    print("Máximo:", resultado[4])
+
+    # Guardar los resultados en un archivo de texto
+    with open("EJERCICIO 1/resultados/resultados_estadisticos_otra_columna.txt", 'w') as f:
+        f.write(f"Promedio: {resultado[0]}\n")
+        f.write(f"Mediana: {resultado[1]}\n")
+        f.write(f"Desviacion estandar: {resultado[2]}\n")
+        f.write(f"Minimo: {resultado[3]}\n")
+        f.write(f"Maximo: {resultado[4]}\n")
 
 # Cargar los datos del archivo CSV
-datos = pd.read_csv("EJERCICIO 1/datos/activaciones_samur_2023(1).csv", sep=';', encoding='ISO-8859-1')
+datos = pd.read_csv("EJERCICIO 1/resultados/datos_con_duracion.csv", sep=';', encoding='ISO-8859-1')
+
+# Seleccionar la columna deseada
+datos_columna = datos['Duracion Intervencion (min)']  # Reemplaza 'Nombre de la columna' por el nombre real
 
 # Crear instancia del producto concreto (en este caso, ResumenEstadistico)
 resumen_estadistico = ResumenEstadistico()
 
 # Interactuar con los datos utilizando el producto concreto
-interactuar_con_datos(resumen_estadistico, datos)
+interactuar_con_datos(resumen_estadistico, datos_columna)
+
+
+
+
+
+
+
