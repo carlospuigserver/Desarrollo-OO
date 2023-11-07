9da56649c27887f3bb3ed660326943da83dac80e
carlospuigserver <cpuiggut@myuax.com>
2023-11-07 13:43:37 +0100

    comit

--- EJERCICIO 1/scripts/modelado_estructuracion.py
+++ EJERCICIO 1/scripts/modelado_estructuracion.py
@@ -6,7 +6,7 @@ data = pd.read_csv("EJERCICIO 1/resultados/datos_preprocesados.csv", sep=';', en
 # Realizar modelado y estructuración para análisis
 
 # Ejemplo: Crear una nueva columna calculando la duración de la intervención
-data['Duracion Intervencion (min)'] = (pd.to_datetime(data['Hora Intervención']) - pd.to_datetime(data['Hora Solicitud'])).dt.total_seconds() / 60
+data['Duracion Intervencion (min)'] = (pd.to_datetime(data['Hora Intervencion']) - pd.to_datetime(data['Hora Solicitud'])).dt.total_seconds() / 60
 
 # Guardar resultados del modelado o estructuración si es necesario
 data.to_csv('EJERCICIO 1/resultados/datos_con_duracion.csv', sep=';', encoding='ISO-8859-1', index=False)
