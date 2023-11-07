5f9167044583249ff4ca94c38e3ce4d069f4027a
carlospuigserver <cpuiggut@myuax.com>
2023-11-07 23:20:51 +0100

    comit

--- EJERCICIO 1/AbstractFactoryMethod/Fabrica Grafica/fabrica_grafica.py
+++ EJERCICIO 1/AbstractFactoryMethod/Fabrica Grafica/fabrica_grafica.py
@@ -26,31 +26,33 @@ class Histograma(GraficosFactory):
         plt.show()
 
     def crear_grafico_barras(self, datos):
-        pass
-
-# Producto Concreto para Gráfico de Barras
-class GraficoBarras(GraficosFactory):
-    def crear_histograma(self, datos):
-        pass
-
-    def crear_grafico_barras(self, datos):
-        plt.bar(range(len(datos)), datos)
+        datos.plot(kind='bar')
         plt.title('Gráfico de Barras')
+
+        # Ajustes estéticos para el eje Y
+        plt.yticks(fontsize=10)  # Tamaño de fuente del eje Y
+        plt.gca().yaxis.grid(True)  # Líneas de la cuadrícula para el eje Y
+
+        # Mejorar la apariencia del eje Y en el gráfico de barras
+        plt.gca().set_ylim([0, datos.max() * 1.1])  # Ajustar límites del eje Y
+        plt.gca().yaxis.grid(True)  # Mostrar líneas de cuadrícula en el eje Y
+        plt.gca().tick_params(axis='y', labelrotation=0)  # Rotar las etiquetas del eje Y
+        plt.tight_layout()  # Ajustar el espaciado para evitar superposición
+
         plt.show()
 
 # Cargar los datos del archivo CSV
 datos = pd.read_csv("EJERCICIO 1/resultados/datos_con_duracion.csv", sep=';', encoding='ISO-8859-1')
 
-# Seleccionar la columna deseada
-datos_columna = datos['Duracion Intervencion (min)']  # Reemplaza 'Nombre de la columna' por el nombre real
+# Seleccionar las primeras 100 filas de la columna deseada
+datos_columna = datos['Duracion Intervencion (min)'].head(30)  # Reemplaza 'Nombre de la columna' por el nombre real y limita a 100 filas
 
 # Usar la fábrica para calcular estadísticas
 resultados = EstadisticasFactory.calcular_estadisticas(datos_columna)
 
 # Crear instancias de las fábricas y productos concretos
 fabrica_histograma = Histograma()
-fabrica_grafico_barras = GraficoBarras()
 
 # Interactuar con los datos utilizando la fábrica de visualizaciones gráficas
 fabrica_histograma.crear_histograma(datos_columna)
-fabrica_grafico_barras.crear_grafico_barras(datos_columna)
+fabrica_histograma.crear_grafico_barras(datos_columna)
