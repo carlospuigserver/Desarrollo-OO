c903128178273f09576821e7983c496ff696c25c
carlospuigserver <cpuiggut@myuax.com>
2023-11-07 14:51:20 +0100

    comit

--- EJERCICIO 1/AbstractFactoryMethod/fabrica_estadisticas.py
+++ EJERCICIO 1/AbstractFactoryMethod/fabrica_estadisticas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from abc import ABC, abstractmethod
+import tkinter as tk
+from tkinter import Label
 
 # Fábrica Abstracta para Análisis Estadísticos
 class EstadisticasFactory(ABC):
@@ -22,21 +24,23 @@ class ResumenEstadistico(EstadisticasFactory):
 # Función que maneja la interacción entre el patrón Abstract Factory y el conjunto de datos
 def interactuar_con_datos(producto, datos):
     resultado = producto.crear_resumen_estadistico(datos)
-    # Presentar los resultados de la manera que desees
-    print("Resultados estadísticos de la Duracion Intervencion (min):")
-    print("Promedio:", resultado[0])
-    print("Mediana:", resultado[1])
-    print("Desviaciíon estándar:", resultado[2])
-    print("Mínimo:", resultado[3])
-    print("Máximo:", resultado[4])
 
-    # Guardar los resultados en un archivo de texto
-    with open("EJERCICIO 1/resultados/resultados_estadisticos_otra_columna.txt", 'w') as f:
-        f.write(f"Promedio: {resultado[0]}\n")
-        f.write(f"Mediana: {resultado[1]}\n")
-        f.write(f"Desviacion estandar: {resultado[2]}\n")
-        f.write(f"Minimo: {resultado[3]}\n")
-        f.write(f"Maximo: {resultado[4]}\n")
+    root = tk.Tk()
+    root.title("Resultados Estadísticos")
+
+    etiquetas = [
+        f"Promedio: {resultado[0]}",
+        f"Mediana: {resultado[1]}",
+        f"Desviación Estándar: {resultado[2]}",
+        f"Mínimo: {resultado[3]}",
+        f"Máximo: {resultado[4]}"
+    ]
+
+    for i, etiqueta_texto in enumerate(etiquetas):
+        etiqueta = Label(root, text=etiqueta_texto)
+        etiqueta.grid(row=i, column=0, sticky='w')
+
+    root.mainloop()
 
 # Cargar los datos del archivo CSV
 datos = pd.read_csv("EJERCICIO 1/resultados/datos_con_duracion.csv", sep=';', encoding='ISO-8859-1')
@@ -49,10 +53,3 @@ resumen_estadistico = ResumenEstadistico()
 
 # Interactuar con los datos utilizando el producto concreto
 interactuar_con_datos(resumen_estadistico, datos_columna)
-
-
-
-
-
-
-
