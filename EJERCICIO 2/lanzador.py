8ba1639ef75bd94dadfe529faff03d412c03d348
carlospuigserver <cpuiggut@myuax.com>
2023-11-08 20:06:53 +0100

    comit

--- EJERCICIO 2/lanzador.py
+++
@@ -1,7 +0,0 @@
-import csv
-from Builder import Masa
-from Builder import MasaBuilder
-from Builder import SalsaBase
-from Builder import SalsaBuilder
-from Builder import Ingrediente
-from Builder import IngredienteBuilder
